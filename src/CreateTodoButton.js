ac5dfcc03d9a5a35a7747d95ba1f0e53788104b3
Alejandro S. Giraldo <alejosegi@gmail.com>
2022-01-03 20:19:34 +0100

    Assign events

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,17 @@ import React from "react";
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
+    const onClickButton = (msg) => {
+        alert(msg);
+    }
+
     return (
-        <button className="CreateTodoButton">+</button>
+        <button
+            className="CreateTodoButton"
+            onClick={() => onClickButton('Aquí se debería mostrar el modalsito')}
+        >
+            +
+        </button>
     );
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,16 @@ import React from "react";
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value)
+    };
+
     return (
-        <input placeholder="Cebolla" className="TodoSearch" />
+        <input
+            placeholder="Cebolla"
+            className="TodoSearch"
+            onChange={onSearchValueChange}
+        />
     );
 }
 
