5f2dbdf268a323ace8a4ac6b25627da38c212e37
Alejandro S. Giraldo <alejosegi@gmail.com>
2022-01-03 13:19:42 +0100

    Inline styles

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,13 @@
 import React from "react";
 
+const estilos = {
+    color: 'red',
+    backgroundColor: 'yellow'
+};
+
 function TodoCounter() {
     return (
-        <h2>Has completado 2 de 3 TODOs</h2>
+        <h2 style={estilos}>Has completado 2 de 3 TODOs</h2>
     );
 }
 
